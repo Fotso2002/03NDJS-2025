e9b895ad0bbf3a66a96d1dc43f25abc1169707e0
Fotso2002 <franckulrich16@gmail.com>
2025-04-14 12:28:48 +0200

    Function for postivenumbe

---
+++ Day 1/sandbox/function.js
@@ -0,0 +1,14 @@
+// function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+// function positive or negative
+function positiveOrNegative(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
