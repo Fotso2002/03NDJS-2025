db42a6275e73e15bd65eeb26d6308f31abf1549d
Fotso Franck <110812239+Fotso2002@users.noreply.github.com>
2025-04-14 11:22:10 +0200

    Creation of objects and array

---
+++ Day 1/sandbox/variable.js
@@ -0,0 +1,12 @@
+// Déclaration d'une variable avec let
+let message = "Bonjour, monde!";
+console.log(message);
+
+// Declaration d'un objet avec let
+let personne = {
+    nom: "Franck",
+    age: 23,
+    ville: "liege"
+};
+// Creation d'un tableau avec let
+let fruits = ["pomme", "banane", "orange"];
