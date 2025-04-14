55306a32c09dd786d8767c104f69cef485d1c853
Fotso Franck <110812239+Fotso2002@users.noreply.github.com>
2025-04-14 11:07:42 +0200

    Introduction to NodeJS

---
+++ Day 1/sandbox/while_loop.js
@@ -0,0 +1,19 @@
+// Statement
+for( let i = 0; i < 11; i + 1){
+    console.log(i);
+}
+
+// do statement while loop
+let i = 0;
+do{
+    1+= 1;
+    console.log(i);
+} while (i < 5);
+
+// while (condition)
+let n = 0;
+let x = 0;
+while (n < 3){
+    n++;
+    x += 0;
+}
