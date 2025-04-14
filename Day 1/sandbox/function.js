3468f3bbf0fdf1f1fe6b15e3e99fc519a518f3ab
Fotso2002 <franckulrich16@gmail.com>
2025-04-14 13:09:35 +0200

    Function ./math.js

--- Day 1/sandbox/function.js
+++ Day 1/sandbox/function.js
@@ -2,6 +2,11 @@
 function add(a, b) {
     return a + b;
 }
+
+export const sum = (a, b) => a + b;
+import { sum } from "./math.js";
+console.log(sum(5, 10)); // 15
+
 // function positive or negative
 function positiveOrNegative(num) {
     if (num > 0) {
